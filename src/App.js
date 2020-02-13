a5208664b0bd7dff434a54e49c50f1cea71ad046
szubi <w.szubi@gmail.com>
2020-02-13 23:23:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import AddProject from './components/project/AddProject';
+import { Provider } from "react-redux"
+import store from "./store"
+import UpdateProject from './components/project/UpdateProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/addProject" component={AddProject} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/updateProject/:id" component={UpdateProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
